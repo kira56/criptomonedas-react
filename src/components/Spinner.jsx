c8be58b76e754cda69fed95e8edb2f59e185e4dc
kira56 <ridhunter1508@gmail.com>
2020-08-02 20:55:24 -0500

    commit Diego

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Spinner.css';
+
+const Spiner = () => {
+  return (
+    <div className="spinner">
+      <div className="dot1"></div>
+      <div className="dot2"></div>
+    </div>
+  );
+};
+
+export default Spiner;
